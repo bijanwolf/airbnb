c4106d450d63c40f262af90ba64a8c04c6d10cc5
bijanwolf <bijanwolf@gmail.com>
2022-08-28 14:39:18 +0200

    started guest sign up

--- src/main.py
+++ src/main.py
@@ -1,14 +1,13 @@
 def main():
-    while True:
-        choice = input(
-            """
-            Are you a Guest or a Host
-            1. Host
-            2. Guest
-            """
-        )
+    #while True:
+        choice = input("""1. is Host
+2. is Guest
+choose 1/2 ?"""
+                    )
 
         if choice == "1":
             host_menu()
         elif choice == "2":
             guest_menu()
+
+main()
