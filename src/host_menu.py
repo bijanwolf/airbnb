81eebb166f064f0a1f03123546a68d348e64f34e
bijanwolf <bijanwolf@gmail.com>
2022-10-19 19:52:54 +0200

    trying to integrate signup fun in host-func w.o. global

--- src/host_menu.py
+++ src/host_menu.py
@@ -7,7 +7,6 @@ file_name = 'host_data'
 def sign_up():
         #db = database
         username = input("Create Username:")
-        global password
         password = input("Create Password:")
         password1 = input("Confirm Password:")
 
@@ -16,23 +15,31 @@ def sign_up():
             sign_up()
         else: 
             print("""Account created successfully
-finish sign in""")
-            global sign_name
+            finish sign in""")
             sign_name = input("Your first name:")
-            global sign_mail 
             sign_mail = input("Your e-mail address:")
-            global sign_address 
             sign_address = input("Your streetname:")
+            
+            res_dict = {}
+            res_dict.update({sign_mail: sign_mail, sign_name: sign_name, sign_address: sign_address, password: password})
+            return res_dict
 
 
 def host_menu():
+    password = ""
+    sign_name = ""
+    sign_mail = ""
+    sign_address = ""
+    
+    
+
     if not os.path.isfile(file_name):
     #   else loadfile, then preceed
         sign_up()
 
         host_dict = {}
 
-        host_dict.update({sign_mail:Hosts(sign_name, sign_mail, sign_address)})
+        host_dict.update({sign_mail:Hosts(sign_name, password, sign_address)})
             
             #pickle gues_dict
         pickle.dump(host_dict,open(file_name,'wb'))
@@ -40,9 +47,15 @@ def host_menu():
     else: 
         # load_pickle_file
         host_data = pickle.load( open (file_name, 'rb'))   
-        sign_up()
-            
-        host_data.update({sign_mail:Hosts(sign_name, sign_mail, sign_address)})
+
+        sign_up_res = sign_up()
+        print(sign_up_res[sign_name])
+        sign_name = sign_up_res[sign_name]
+        password =  sign_up_res[password]
+        sign_mail = sign_up_res[sign_mail]
+        sign_address = sign_up_res[sign_address]
+
+        host_data.update({sign_mail:Hosts(sign_name, password, sign_address)})
             
             #pickle gues_dict
         pickle.dump(host_data,open(file_name,'wb'))
--- src/main.py
+++ src/main.py
@@ -2,19 +2,19 @@ from host_menu import sign_up, host_menu
 from guest_menu import sign_up, guest_menu
 
 def main():
-    opt = True
-    while opt == True: 
-        input_1 = input("Do you already have an account (y/n)?")
-        if input_1 == 'y':
-            #login
-            pass
-        #sign_up
-        elif input_1 == 'n':
-            choice = input("1. is Host 2. is Guest choose 1/2 ?")
-            if choice == '1':
-                host_menu()
-            elif choice == '2':
-                guest_menu()
+    # opt = True
+    # while opt == True: 
+    #     input_1 = input("Do you already have an account (y/n)?")
+    #     if input_1 == 'y':
+    #         #login
+    #         pass
+    #     #sign_up
+    #     elif input_1 == 'n':
+    #         choice = input("1. is Host 2. is Guest choose 1/2 ?")
+    #         if choice == '1':
+    #             host_menu()
+    #         elif choice == '2':
+                # guest_menu()
             # host 
         # if choice == "1":
         # host_menu()
