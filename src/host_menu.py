ec0be74e15b0b28eb4758cae0a84d54f447e948d
bijanwolf <bijanwolf@gmail.com>
2022-10-23 17:23:39 +0200

    sign_up func works with host_func, but does not pickle data anymore

--- src/host_menu.py
+++ src/host_menu.py
@@ -4,24 +4,26 @@ from classes.user import Hosts, User
 
 file_name = 'host_data'
 
-def sign_up():
+def sign_up(x,y,z, a, b, c):
         #db = database
-        username = input("Create Username:")
-        password = input("Create Password:")
-        password1 = input("Confirm Password:")
+        x = input("Create Username:")
+        y = input("Create Password:")
+        z = input("Confirm Password:")
 
-        if password != password1:
+        if y != z:
             print("Password does not match! You need to restart.")
             sign_up()
         else: 
             print("""Account created successfully
             finish sign in""")
-            sign_name = input("Your first name:")
-            sign_mail = input("Your e-mail address:")
-            sign_address = input("Your streetname:")
+            a = input("Your first name:")
+            b = input("Your e-mail address:")
+            c = input("Your streetname:")
             
             res_dict = {}
-            res_dict.update({sign_mail: sign_mail, sign_name: sign_name, sign_address: sign_address, password: password})
+            res_dict.update({'sign_mail': b, 'sign_name': a, 'sign_address': c, 'password': x})
+            print('############################################')
+            print(res_dict)
             return res_dict
 
 
@@ -30,7 +32,8 @@ def host_menu():
     sign_name = ""
     sign_mail = ""
     sign_address = ""
-    
+    username = ""
+    password1 = ""
     
 
     if not os.path.isfile(file_name):
@@ -48,15 +51,16 @@ def host_menu():
         # load_pickle_file
         host_data = pickle.load( open (file_name, 'rb'))   
 
-        sign_up_res = sign_up()
-        print(sign_up_res[sign_name])
-        sign_name = sign_up_res[sign_name]
-        password =  sign_up_res[password]
-        sign_mail = sign_up_res[sign_mail]
-        sign_address = sign_up_res[sign_address]
+        sign_up_res = sign_up(username, password, password1, sign_name, sign_mail, sign_address)
+        
+        print(sign_up_res['sign_name'])
+        sign_name = sign_up_res['sign_name']
+        password =  sign_up_res['password']
+        sign_mail = sign_up_res['sign_mail']
+        sign_address = sign_up_res['sign_address']
 
         host_data.update({sign_mail:Hosts(sign_name, password, sign_address)})
-            
+        print(host_data) 
             #pickle gues_dict
         pickle.dump(host_data,open(file_name,'wb'))
             # outfile.close()   
